ec23a25582c79f7d1b26a4d8d9d442649c2e9e02
WreshZ98 <129671203+WreshZ98@users.noreply.github.com>
2024-06-13 18:07:27 +0000

    Update

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, send_file, abort
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Example mapping of keys to filenames
+key_to_filename = {
+    '01': 'file1.txt',
+    '02': 'file2.txt',
+}
+
+@app.route('/getfile/<key>')
+def get_file(key):
+    filename = key_to_filename.get(key)
+    if filename:
+        path = "/workspaces/DataCoinServer/"+filename
+        print(path)
+        return send_file(path, as_attachment = True)
+        #return send_from_directory(directory='/', filename=filename)
+    else:
+        abort(404, description="Key not found")
+
+if __name__ == '__main__':
+    app.run(debug=True)

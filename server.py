5d20d897d890dab1d4f7ddfa1cbae6d384991721
WreshZ98 <129671203+WreshZ98@users.noreply.github.com>
2024-06-14 16:53:17 +0000

    Update

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, send_file, abort
-import os
+from flask import Flask, request, jsonify, send_file
 from supabase import create_client, Client
 from typing import cast
-app = Flask(__name__)
+import os
+import numpy as np
+import pandas as pd
 
-# Example mapping of keys to filenames
-key_to_filename = {
-    '01': 'file1.txt',
-    '02': 'file2.txt',
-}
+np.random.seed(42)
+
+random_matrix = np.random.rand(5, 3)
+
+df = pd.DataFrame(random_matrix)
+
+app = Flask(__name__)
 
 url = os.environ.get("SUPABASE_URL")
 url = cast(str, url)
@@ -18,22 +21,30 @@ supabase: Client = create_client(url, key )
 
 response = supabase.table('Users').select("*").execute()
 
-@app.route('/getfile/<key>')
-def get_file(key):
-    filename = key_to_filename.get(key)
-    if filename:
-        path = "/workspaces/DataCoinServer/"+filename
-        print(path)
-        return send_file(filename, as_attachment = True)
-        #return send_from_directory(directory='/', filename=filename)
-    else:
-        abort(404, description="Key not found")
 
+@app.route('/login', methods=['GET'])
+def get_data():
+    # Get the integer parameter from the query string
+    id = request.args.get('id', type=int)
+    password = request.args.get('password', type=int)
+    action = request.args.get('action', type=str)
+    if id is not None and password is not None:
+        response = supabase.table('Users').select("id, password").eq("id", id).execute()
+        print(type(response.data))
+        if password == response.data[0].get("password"):
+            print("cascanueces")
+            if action is not None and action != "NONE":
+                print("doble cascanueces")
+                if action == "GET_TASK":
+                    df.to_csv("data.csv")
+                    return send_file("data.csv", mimetype='text/csv', as_attachment=True, download_name='data.csv')
+        return jsonify({"received_number": response.data[0]})
+    else:
+        return jsonify({"error": "No number provided"}), 400
 
 @app.route("/")
 def hello_world():
     return f'{response}\'s profile'
-from markupsafe import escape
 
 if __name__ == '__main__':
     print(response)

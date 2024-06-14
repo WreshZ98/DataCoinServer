e2035605ba421cbcd72a84f69391576d72344ad3
WreshZ98 <129671203+WreshZ98@users.noreply.github.com>
2024-06-14 15:46:56 +0000

    Update

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_file, abort
-
+import os
+from supabase import create_client, Client
+from typing import cast
 app = Flask(__name__)
 
 # Example mapping of keys to filenames
@@ -7,6 +9,14 @@ key_to_filename = {
     '01': 'file1.txt',
     '02': 'file2.txt',
 }
+
+url = os.environ.get("SUPABASE_URL")
+url = cast(str, url)
+key = os.environ.get("SUPABASE_KEY")
+key = cast(str,key)
+supabase: Client = create_client(url, key )
+
+response = supabase.table('Users').select("*").execute()
 
 @app.route('/getfile/<key>')
 def get_file(key):
@@ -19,5 +29,12 @@ def get_file(key):
     else:
         abort(404, description="Key not found")
 
+
+@app.route("/")
+def hello_world():
+    return f'{response}\'s profile'
+from markupsafe import escape
+
 if __name__ == '__main__':
+    print(response)
     app.run(debug=True)
